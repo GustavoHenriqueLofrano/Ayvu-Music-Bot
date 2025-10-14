4054450d96780656e82d66d3c1f532d2705cff92
Gustavo Lofrano <gustavohenriquelofrano@hotmail.com>
2025-10-14 10:22:19 -0300

    add innertube

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { DefaultExtractors } from "@discord-player/extractor"
 import { SpotifyExtractor } from "discord-player-spotify";
 import { Player } from "discord-player";
 import { YoutubeiExtractor } from "discord-player-youtubei";
+import { Innertube } from "youtubei.js";
 import createPlayingNowEvent from "./discord/events/playingNow.js"
 import createDisconnectEvent from "./discord/events/disconnect.js";
 import { Client, GatewayIntentBits, Partials } from "discord.js";
@@ -33,6 +34,16 @@ const player = new Player(client as never, {
   blockStreamFrom: [],
   blockExtractors: []
 });
+
+(async () => {
+  try {
+    const yt = await Innertube.create();
+    console.log("✅ YouTube Innertube iniciado:", yt.session.context.client.clientVersion);
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
 
 // Promise para carregar os extractors
 await Promise.all([
